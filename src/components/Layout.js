9a2033347e6f26c92ca8446868c4deb9329076a1
Damian Mullins <damian.mullins@just-eat.com>
2020-11-06 23:08:13 +0000

    v4.19.0 - Update prism theme.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,7 @@ import * as Sentry from '@sentry/browser';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-import 'prismjs/themes/prism-funky.css';
+import 'prism-themes/themes/prism-material-dark.css';
 import '../styles/global.module.scss';
 import layoutStyles from '../styles/layout.module.scss';
 
