2f541f6b80803cb2dc45558c67cb791bbba6f643
Damian Mullins <damian.mullins@just-eat.com>
2020-11-07 12:02:28 +0000

    Updates.

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,7 +102,7 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          'gatsby-remark-autolink-headers',
+          'gatsby-remark-heading-slug',
           'gatsby-remark-prismjs',
           {
             resolve: `gatsby-remark-copy-linked-files`,
