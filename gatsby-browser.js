4778d1fdb4157b3581283ea861cffcdb5db854eb
Damian Mullins <damian.mullins@just-eat.com>
2020-11-10 20:22:29 +0000

    v4.22.0 - Fix LogRocket error.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,28 +7,26 @@ import { Integrations } from "@sentry/tracing";
 const environment = process.env.NODE_ENV;
 const version = process.env.GATSBY_RELEASE_VERSION;
 
-
 if (environment === 'production') {
-    LogRocket.init('u8t5r0/damianmullinscom');
+  LogRocket.init('u8t5r0/damianmullinscom');
 
-    setupLogRocketReact(LogRocket);
+  setupLogRocketReact(LogRocket);
 
-    Sentry.init({
-        dsn: "https://b7ebba6ca5dd4d65a2ee0ea7f7665a22@o43921.ingest.sentry.io/1197101",
-        environment,
-        release: `damianmullins@${version}`,
+  Sentry.init({
+    dsn: "https://b7ebba6ca5dd4d65a2ee0ea7f7665a22@o43921.ingest.sentry.io/1197101",
+    environment,
+    release: `damianmullins@${version}`,
 
-        integrations: [
-            new Integrations.BrowserTracing(),
-        ],
+    integrations: [
+      new Integrations.BrowserTracing(),
+    ],
 
-        tracesSampleRate: 1.0
-    });
+    tracesSampleRate: 1.0
+  });
 
+  LogRocket.getSessionURL(sessionURL => {
     Sentry.configureScope(scope => {
-      scope.addEventProcessor(async event => {
-        event.extra.sessionURL = LogRocket.sessionURL;
-        return event;
-      });
+      scope.setExtra("sessionURL", sessionURL);
     });
+  });
 }
