05152fa57355d2ef258a95780b18ddb84a59ed96
Damian Mullins <89059+DamianMullins@users.noreply.github.com>
2020-11-10 22:18:07 +0000

    V4.29.0 (#45)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -15,9 +15,10 @@ if (environment === 'production') {
   Sentry.init({
     dsn: "https://b7ebba6ca5dd4d65a2ee0ea7f7665a22@o43921.ingest.sentry.io/1197101",
     environment,
-    release: `damianmullins@${release}`,
+    release,
 
     integrations: [
+      new Sentry.Integrations.UserAgent(),
       new Integrations.BrowserTracing(),
     ],
 
